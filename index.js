02ce6e489f3837ca9b2b67dc0f394f451f427835
Student <example@example.com>
2020-02-24 16:30:06 -0800

    add dispatch mathods and more

--- index.js
+++ index.js
@@ -1,8 +1,17 @@
 import React from 'react';
 import { render } from 'react-dom';
 import Scoreboard from './src/containers/Scoreboard';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import PlayerReducer from './src/reducers/player';
+
+const store = createStore(
+  PlayerReducer
+);
 
 render(
-  <Scoreboard />,
+  <Provider store={store}>
+  <Scoreboard />
+  </Provider>,
   document.getElementById('root')
 );
